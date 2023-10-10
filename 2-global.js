a6431c9cbd284fbbb4f61b5a897a0e68dbb59364
Muhammad Sohaib <muhammadsohaib030@gmail.com>
2023-10-10 20:51:44 +0500

    global varibles and etc..

---
+++ 2-global.js
@@ -0,0 +1,20 @@
+//  Globals  - NO WINDOW !!!
+
+//  __dirname - Path to current directory
+//  __filename - file name
+//  require - function to use modules (CommonJs)
+//  module - info about current modeule (file)
+//  process - info about environment where the program is being executed
+
+
+console.log(__dirname);
+console.log(__filename);
+
+setInterval(() => {
+    console.log("John Smilga");
+}, 1000);
+
+setTimeout(() => {
+    console.log("Sohaib");
+}, 3);
+
